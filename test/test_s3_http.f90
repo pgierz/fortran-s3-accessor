c4bd61cfd4add1cad31c520909104b5cea382e85
Paul Gierz <pgierz@awi.de>
2025-09-28 16:59:31 +0200

    Add mock curl testing infrastructure
    - Create test/scripts/curl mock script with F90S3_TEST_MODE support
    - Add test/data/responses/ with mock S3 response files
    - Implement file-based mock responses for GET/HEAD operations
    - Add test_get_object() test that uses real s3_http functions
    - Mock system intercepts curl calls transparently
    - All tests passing with mocked S3 responses

--- test/test_s3_http.f90
+++ test/test_s3_http.f90
@@ -14,7 +14,8 @@ contains
         type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
         testsuite = [ &
-            new_unittest("config_init", test_config_init) &
+            new_unittest("config_init", test_config_init), &
+            new_unittest("get_object", test_get_object) &
         ]
     end subroutine collect_s3_http
 
@@ -38,5 +39,31 @@ contains
 
         call check(error, config%use_https .eqv. .true., "Config should use HTTPS")
     end subroutine test_config_init
+
+    !> Test S3 get_object function
+    subroutine test_get_object(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+        type(s3_config) :: config
+        character(len=:), allocatable :: content
+        logical :: success
+
+        ! Initialize S3 config
+        config%bucket = 'test-bucket'
+        config%endpoint = 's3.amazonaws.com'
+        config%use_https = .true.
+        call s3_init(config)
+
+        ! Test getting an object that should exist (in mock)
+        success = s3_get_object("test_object.txt", content)
+
+        call check(error, success, "Should successfully get test_object.txt")
+        if (allocated(error)) return
+
+        call check(error, allocated(content), "Content should be allocated")
+        if (allocated(error)) return
+
+        call check(error, len(content) > 0, "Content should not be empty")
+    end subroutine test_get_object
 
 end module test_s3_http
